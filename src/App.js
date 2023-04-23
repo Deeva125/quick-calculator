33043e38dd1d1a6ab640b60765896107878413a2
Deeva Singhal <108374249+Deeva125@users.noreply.github.com>
2023-04-23 23:31:01 +0000

    Improved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import Square from "./Components/Square"
-import SquareRoot from './Components/SquareRoot';
+import { useState } from 'react';
 function App() {    
+  const [number, setNumber] = useState(0)
+  const handleChange = (event) =>{
+    setNumber(event.target.value)
+  }
     return (
     <div className="App">
-      <Square/>
-      <SquareRoot/>
+  <input onChange={handleChange} value={number}/>
+  <div>
+    <h3>Square</h3>
+    <p>{number*number}</p>
+  </div>
+  <div>
+    <h3>Square Root</h3>
+    <p>{number**0.5}</p>
+  </div>
+  <div>
+    <h3>Cube</h3>
+    <p>{number**3}</p>
+  </div>
+  <div>
+    <h3>Cube Root</h3>
+    <p>{number**(1/3)}</p>
+  </div>
     </div>
   );
 }
