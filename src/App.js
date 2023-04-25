a35c4aef071c035f0b417e645801ada217e21811
Deeva Singhal <108374249+Deeva125@users.noreply.github.com>
2023-04-25 23:44:41 +0000

    Changed layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
 import './App.css';
 import { useState } from 'react';
-function App() {    
+function App() {
   const [number, setNumber] = useState(0)
-  const handleChange = (event) =>{
+  const handleChange = (event) => {
     setNumber(event.target.value)
   }
-    return (
+  return (
     <div className="App">
-  <input onChange={handleChange} value={number}/>
-  <div>
-    <h3>Square</h3>
-    <p>{number*number}</p>
-  </div>
-  <div>
-    <h3>Square Root</h3>
-    <p>{number**0.5}</p>
-  </div>
-  <div>
-    <h3>Cube</h3>
-    <p>{number**3}</p>
-  </div>
-  <div>
-    <h3>Cube Root</h3>
-    <p>{number**(1/3)}</p>
-  </div>
+      <h1>Quick Calculator</h1>
+      <input className='w-75 mt-3' onChange={handleChange} value={number} />
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col'>
+            <div class="card w-75 m-auto">
+              <div class="card-body">
+                <h5 class="card-title">Square</h5>
+                <p class="card-text">{number * number}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col'>
+            <div class="card w-75 m-auto">
+              <div class="card-body">
+                <h5 class="card-title">Square Root</h5>
+                <p class="card-text">{number ** 0.5}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col'>
+            <div class="card w-75 m-auto">
+              <div class="card-body">
+                <h5 class="card-title">Cube</h5>
+                <p class="card-text">{number ** 3}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col'>
+            <div class="card w-75 m-auto">
+              <div class="card-body">
+                <h5 class="card-title">Cube Root</h5>
+                <p class="card-text">{number **(1/3)}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
